3efeef66ba5dfa5f9aa38c75239db8fdd5218d01
AnindyaBsws <shakhar004@gmail.com>
2025-04-11 04:40:53 +0530

    React basic is added

---
+++ 01basicsreact/src/App.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+    <h1>Yo whats up?</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,10 @@
+
+
+function App() {
+
+  return (
+    <h1>Baby yohoo</h1>
+  )
+}
+
+export default App
